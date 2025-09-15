e13bae675e1009e85717ec7a37cf4b2130426080
Robin Temme <robin@temme.cloud>
2025-09-15 19:01:13 +0200

    Add kris vcard redirect

--- static/js/redirect-vcard.js
+++ static/js/redirect-vcard.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const shouldRedirect =
+  const shouldRedirectRobin =
     window.location.pathname === "/qr-visitenkarte-v1/" ||
     document.body.dataset.redirect === "vcard";
 
-  if (shouldRedirect) {
+  if (shouldRedirectRobin) {
     setTimeout(function () {
       window.location.href = "/robin.vcf";
+    }, 8000);
+  }
+
+  const shouldRedirectKris = window.location.pathname === "/qr-visitenkarte-v2/";
+
+  if (shouldRedirectKris) {
+    setTimeout(function () {
+      window.location.href = "/kris.vcf";
     }, 8000);
   }
 });
